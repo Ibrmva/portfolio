ae9a809c4f7610de698628fc4b3f1f5db42fdcea
Gulaiym <90143818+Ibrmva@users.noreply.github.com>
2022-10-27 22:31:25 +0600

    Update script.js

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -16,3 +16,18 @@ button.onclick = function() {
   count += 1;
   button.innerHTML = "Click me: " + count;
 };
+fetch('https://reqres.in/api/users',{
+     method : 'POST',
+     headers: {
+         'Content-Type':'application/json'
+     },
+     body: JSON.stringify({
+         name: 'User 1'
+     })
+ })
+ .then(res => {
+     return res.json( )
+ })
+
+ .then(data => console.log(data))
+ .catch(error => console.log('ERROR'))
